9370e6422b47bdf8af3e9d9cf6424c9adb4a8d92
TrongDungEmb98 <ntdungembedded98@gmail.com>
2018-07-27 16:53:02 +0700

    Configuration uart is success - Error is configuration clock fault

--- uart1/src/clock.c
+++ uart1/src/clock.c
@@ -24,44 +24,44 @@ void SystemInit(void)
 	
 	/* Reset SW[1:0], HPRE[3:0], PPRE[2:0], ADCPRE, MCOSEL[2:0], MCOPRE[2:0] and PLLNODIV bits */
 	//RCC->CFGR &= (uint32_t)0x08FFB80CU;
-	temp_reg = read_reg(RCC_CFGR, ~0x08FFB80Cu);
+	temp_reg = read_reg(RCC_CFGR, 0x08FFB80Cu);
 	write_reg(RCC_CFGR, temp_reg);
 	
 	/* Reset HSEON, CSSON and PLLON bits */
 	//RCC->CR &= (uint32_t)0xFEF6FFFFU;
-	temp_reg = read_reg(RCC_CR, ~0xFEF6FFFFU);
+	temp_reg = read_reg(RCC_CR, 0xFEF6FFFFU);
 	write_reg(RCC_CR, temp_reg);
 	
 	/* Reset HSEBYP bit */
 	//RCC->CR &= (uint32_t)0xFFFBFFFFU;
-	temp_reg = read_reg(RCC_CR, ~0xFFFBFFFFU);
+	temp_reg = read_reg(RCC_CR, 0xFFFBFFFFU);
 	write_reg(RCC_CR, temp_reg);
 	
 	/* Reset PLLSRC, PLLXTPRE and PLLMUL[3:0] bits */
 	//RCC->CFGR &= (uint32_t)0xFFC0FFFFU;
-	temp_reg = read_reg(RCC_CFGR,~0xFFC0FFFFU);
+	temp_reg = read_reg(RCC_CFGR,0xFFC0FFFFU);
 	write_reg(RCC_CFGR, temp_reg);
 	
 	/* Reset PREDIV[3:0] bits */
 	//RCC->CFGR2 &= (uint32_t)0xFFFFFFF0U;
-	temp_reg = read_reg(RCC_CFGR2, ~0xFFFFFFF0U);
+	temp_reg = read_reg(RCC_CFGR2, 0xFFFFFFF0U);
 	write_reg(RCC_CFGR2, temp_reg);
 
 	/* Reset USART1SW[1:0], I2C1SW and ADCSW bits */
 	//RCC->CFGR3 &= (uint32_t)0xFFFFFEECU;
-	temp_reg = read_reg(RCC_CFGR3, ~0xFFFFFEECU);
+	temp_reg = read_reg(RCC_CFGR3, 0xFFFFFEECU);
 	write_reg(RCC_CFGR3, temp_reg);
 	
 	/* Reset HSI14 bit */
 	//RCC->CR2 &= (uint32_t)0xFFFFFFFEU;
-	temp_reg = read_reg(RCC_CR2, ~0xFFFFFFFEU);
+	temp_reg = read_reg(RCC_CR2, 0xFFFFFFFEU);
 	write_reg(RCC_CR2, temp_reg);
 	
 	/* Disable all interrupts */
 	//RCC->CIR = 0x00000000U;
 	temp_reg = read_reg(RCC_CIR, ~0x00000000U);
 	write_reg(RCC_CIR, temp_reg);
-}
+} 
 
 /*Enable Clock*/
 void clock_enable(void)
